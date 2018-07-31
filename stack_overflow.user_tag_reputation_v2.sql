1c7695af6a84e267ff64079f7c6d579a4bc8272f
grokify <johncwang@gmail.com>
2018-07-30 21:30:19 -0700

    update style

--- stack_overflow.user_tag_reputation_v2.sql
+++ stack_overflow.user_tag_reputation_v2.sql
@@ -18,7 +18,7 @@ FROM (
       LEFT JOIN `bigquery-public-data.stackoverflow.posts_questions` q
         ON a.parent_id = q.id
     WHERE a.owner_user_id = 1908967
-      AND v.vote_type_id in (1, 2)
+      AND v.vote_type_id IN (1, 2)
       AND DATE(v.creation_date) < DATE('2015-06-29')
   ), UNNEST(tag) AS tag
   GROUP BY tag, vote_type_id
