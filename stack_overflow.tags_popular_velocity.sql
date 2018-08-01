26b4d039b750c965bc268a3d0b7a67fa7e02502a
grokify <johncwang@gmail.com>
2018-08-01 00:53:05 -0700

    simply SQL

--- stack_overflow.tags_popular_velocity.sql
+++ stack_overflow.tags_popular_velocity.sql
@@ -5,18 +5,15 @@ SELECT
   COUNT(tag) AS count_last_3_months,
   CAST(COUNT(tag) / 90 AS INT64) AS count_per_day,
   CAST(COUNT(tag) / 90 / 24 AS INT64) AS count_per_hour
-FROM
-(
+FROM (
   SELECT SPLIT(q.tags, '|') AS tag
   FROM `bigquery-public-data.stackoverflow.posts_questions` q
-  CROSS JOIN
-  (
-    SELECT DATETIME_SUB(DATETIME(creation_date), INTERVAL 3 MONTH) AS start
+  WHERE DATETIME(q.creation_date) >= (
+    SELECT DATETIME_SUB(DATETIME(creation_date), INTERVAL 3 MONTH)
     FROM `bigquery-public-data.stackoverflow.posts_questions`
     ORDER BY creation_date DESC
     LIMIT 1
-  ) AS dates
-  WHERE DATETIME(q.creation_date) >= dates.start
+  )
 ), UNNEST(tag) AS tag
 GROUP BY tag
 ORDER BY count_last_3_months DESC
